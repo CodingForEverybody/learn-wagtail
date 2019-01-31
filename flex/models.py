4ded6de98c6a6eb640e01a1c24f677cddbff3bc7
Kalob Taulien <kalob.taulien@gmail.com>
2019-01-31 08:45:58 -0700

    Added basic flex page

---
+++ flex/models.py
@@ -0,0 +1,26 @@
+"""Flexible page."""
+from django.db import models
+
+from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.core.models import Page
+
+
+class FlexPage(Page):
+    """Flexibile page class."""
+
+    template = "flex/flex_page.html"
+
+    # @todo add streamfields 
+    # content = StreamField()
+
+    subtitle = models.CharField(max_length=100, null=True, blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("subtitle"),
+    ]
+
+    class Meta:  # noqa 
+        verbose_name = "Flex Page"
+        verbose_name_plural = "Flex Pages"
+
+    
--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -26,6 +26,7 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 INSTALLED_APPS = [
     'home',
     'search',
+    'flex',
 
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
