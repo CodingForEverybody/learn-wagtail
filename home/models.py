e280dfac976984972facde4589e84a72329d9290
Kalob Taulien <kalob.taulien@gmail.com>
2019-02-04 10:39:32 -0700

    Oderables lesson code

--- home/models.py
+++ home/models.py
@@ -1,11 +1,34 @@
 from django.db import models
 
-from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel, PageChooserPanel
-from wagtail.core.models import Page
+from modelcluster.fields import ParentalKey
+
+from wagtail.admin.edit_handlers import (
+    FieldPanel,
+    MultiFieldPanel,
+    InlinePanel,
+    StreamFieldPanel,
+    PageChooserPanel,
+)
+from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField, StreamField
 from wagtail.images.edit_handlers import ImageChooserPanel
 
 from streams import blocks
+
+
+class HomePageCarouselImages(Orderable):
+    """Between 1 and 5 images for the home page carousel."""
+
+    page = ParentalKey("home.HomePage", related_name="carousel_images")
+    carousel_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+
+    panels = [ImageChooserPanel("carousel_image")]
 
 
 class HomePage(Page):
@@ -21,29 +44,32 @@ class HomePage(Page):
         null=True,
         blank=False,
         on_delete=models.SET_NULL,
-        related_name="+"
+        related_name="+",
     )
     banner_cta = models.ForeignKey(
         "wagtailcore.Page",
         null=True,
         blank=True,
         on_delete=models.SET_NULL,
-        related_name="+"
+        related_name="+",
     )
 
-    content = StreamField(
-        [
-            ("cta", blocks.CTABlock()),
-        ],
-        null=True,
-        blank=True,
-    )
+    content = StreamField([("cta", blocks.CTABlock())], null=True, blank=True)
 
     content_panels = Page.content_panels + [
-        FieldPanel("banner_title"),
-        FieldPanel("banner_subtitle"),
-        ImageChooserPanel("banner_image"),
-        PageChooserPanel("banner_cta"),
+        MultiFieldPanel(
+            [
+                FieldPanel("banner_title"),
+                FieldPanel("banner_subtitle"),
+                ImageChooserPanel("banner_image"),
+                PageChooserPanel("banner_cta"),
+            ],
+            heading="Banner Options",
+        ),
+        MultiFieldPanel(
+            [InlinePanel("carousel_images", max_num=5, min_num=1, label="Image")],
+            heading="Carousel Images",
+        ),
         StreamFieldPanel("content"),
     ]
 
