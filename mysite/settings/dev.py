4efc0639b89261cdb99ca6fdf3c4a974a4ad2493
Kalob Taulien <kalob.taulien@gmail.com>
2019-06-04 10:35:37 -0600

    Updating Wagtail, Django and Debug Toolbar Wagtail 2.5.1; Django 2.2.2; Debug Toolbar Updated & Disabled

--- mysite/settings/dev.py
+++ mysite/settings/dev.py
@@ -12,13 +12,13 @@ ALLOWED_HOSTS = ['*']
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 INSTALLED_APPS = INSTALLED_APPS + [
-    'debug_toolbar',
+    # 'debug_toolbar',
     'django_extensions',
     'wagtail.contrib.styleguide',
 ]
 
 MIDDLEWARE = MIDDLEWARE + [
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 INTERNAL_IPS = ("127.0.0.1", "172.17.0.1")
