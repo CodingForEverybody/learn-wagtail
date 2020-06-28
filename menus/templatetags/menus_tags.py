44ab16f95310caa34d56467f5ecd1c3ddc33eee6
Kalob Taulien <4743971+KalobTaulien@users.noreply.github.com>
2020-06-28 13:26:37 -0600

    @kevinhowbrook's suggestion to simplify code

--- menus/templatetags/menus_tags.py
+++ menus/templatetags/menus_tags.py
@@ -7,7 +7,4 @@ register = template.Library()
 
 @register.simple_tag()
 def get_menu(slug):
-    try:
-        return Menu.objects.get(slug=slug)
-    except Menu.DoesNotExist:
-        return Menu.objects.none()
+    return Menu.objects.filter(slug=slug).first()
